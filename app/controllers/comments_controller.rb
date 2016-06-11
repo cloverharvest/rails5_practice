727b2bb05ad792ef08d7c3696d19edba95cbe7a1
cloverharvest <anna@schwabdev.com>
2016-06-11 00:47:30 -0700

    Able to make comments on posts, add mailer function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,19 @@
 class CommentsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
+  before_action :set_post
 
   def create
+    comment = @post.comments.create! comments_params
+    CommentsMailer.submitted(comment).deliver_later
+    
+    redirect_to @post
   end
+
+  private
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def comments_params
+      params.required(:comment).permit(:body)
+    end
 end
